6aa95c3395b752c0f6ff223d31f717f4b8857b33
Antje-Claudia <ant.schmollinger.21@lehre.mosbach.dhbw.de>
2022-12-16 16:27:07 +0100

    Added carousel.

--- index.php
+++ index.php
@@ -5,10 +5,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>PHP Projekt</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css"/>
   </head>
   <body>
     <!-- Header mit Navbar -->
-    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-5">
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark">
       <a href="#" class="navbar-brand mb-0 h1">
         <img class="d-inline-block align-top ml-3" src="Logo_sm.png" height="108" width="109" alt="Logo"/>
       </a>
@@ -34,17 +35,44 @@
     </nav>
 
     <!-- Mainbereich mit Inhalten -->
-        <?php
-            $hello = "Hallo Welt";
-            echo $hello;
-            
-        ?>
-    
+    <div id="myCarousel" class="carousel slide carousel-fade" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-item active" style="background-image:url(./berge.jpeg);">
+          <div class="container">
+            <h1 class="text-black">Winter Wunderland</h1>
+            <p class="text-black">Bei meinem Urlaub in den Schweizer Alpen konnte ich<br>die wunderschöne Alpinwelt erleben.</p>
+            <a href="#" class="btn btn-lg btn-info">Zum Beitrag</a>
+          </div>
+        </div>
+        <div class="carousel-item" style="background-image:url(./polarlichter.jpeg);">
+          <div class="container">
+            <h1 class="text-white">Auf den Spuren der Polarlichter</h1>
+            <p class="text-white">Im letzten November habe ich mich auf den Weg gemacht<br>die wunderschönen Polarlichter in Norwegen<br>zu besuchen.</p>
+            <a href="#" class="btn btn-lg btn-info">Zum Beitrag</a>
+          </div>
+        </div>
+        <div class="carousel-item" style="background-image:url(./kueste.jpeg);">
+          <div class="container">
+            <h1 class="text-black">Strandurlaub im Winter</h1>
+            <p class="text-black">Richtig gelesen: Auch im Winter<br>kann so ein Urlaub am Meer wunderschön sein!<br>Wie bei meinem (Winter-)Urlaub in Island.</p>
+            <a href="#" class="btn btn-lg btn-info">Zum Beitrag</a>
+          </div>
+        </div>
+      </div>
     <!-- Footer -->
         <?php
             include "footer.html";
         ?>
 
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
     </body>
 </html>

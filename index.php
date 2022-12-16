dab3b8b3ecb1375c60adc55a16a1845d02cdcfa6
Antje-Claudia <ant.schmollinger.21@lehre.mosbach.dhbw.de>
2022-12-16 13:21:02 +0100

    Included Header and Footer to index.php.

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
+    <!-- Header mit Navbar -->
     <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-5">
       <a href="#" class="navbar-brand mb-0 h1">
         <img class="d-inline-block align-top ml-3" src="Logo_sm.png" height="108" width="109" alt="Logo"/>
@@ -32,28 +33,18 @@
       </div>
     </nav>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    <!-- Mainbereich mit Inhalten -->
         <?php
             $hello = "Hallo Welt";
             echo $hello;
+            
         ?>
+    
+    <!-- Footer -->
+        <?php
+            include "footer.html";
+        ?>
+
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     </body>
 </html>

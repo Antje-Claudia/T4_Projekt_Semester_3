9c8ff70955b0cd81e0e63654f59ea5ea8b629a23
Antje-Claudia <ant.schmollinger.21@lehre.mosbach.dhbw.de>
2022-12-16 16:46:53 +0100

    Added Headline.

--- index.php
+++ index.php
@@ -63,8 +63,9 @@
             <a href="#" class="btn btn-lg btn-info">Zum Beitrag</a>
           </div>
         </div>
-      </div>
-    <!-- Footer -->
+    </div>
+    <h1 class="mb-5 mt-5 text-center">Willkommen auf meinem Blog!</h1>
+      <!-- Footer -->
         <?php
             include "footer.html";
         ?>
@@ -72,7 +73,5 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </body>
-</html>
     </body>
 </html>

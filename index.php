08e27c10ee40617c116fe90dd3796aaf50ee9e4c
Antje-Claudia <ant.schmollinger.21@lehre.mosbach.dhbw.de>
2022-12-16 11:39:05 +0100

    Added Header with Navbar to index.php & login.html

--- index.php
+++ index.php
@@ -7,7 +7,49 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body>
-        <h1>Test<h1>
+    <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-5">
+      <a href="#" class="navbar-brand mb-0 h1">
+        <img class="d-inline-block align-top ml-3" src="Logo_sm.png" height="108" width="109" alt="Logo"/>
+      </a>
+      <button type="button"
+        data-bs-toggle="collapse" 
+        data-bs-target="#navbarNav"
+        class="navbar-toggler text-info"
+        aria-controls="navbarNav" 
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item active">
+            <a href="#" class="nav-link active">Home</a>
+          </li>
+          <li class="nav-item active">
+            <a href="login.html" class="nav-link">Login</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         <?php
             $hello = "Hallo Welt";
             echo $hello;
